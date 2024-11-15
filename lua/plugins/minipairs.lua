c239ad7350473369ba0abbdcb8858759ac64bf4c
jpope6 <jaredpope06@gmail.com>
2024-11-15 08:46:57 -0800

    add more plugins

--- lua/plugins/minipairs.lua
+++ lua/plugins/minipairs.lua
@@ -16,5 +16,6 @@ return {
   config = function(_, opts)
     require("mini.pairs").setup(opts)
   end,
+
 }
 
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,10 +1,17 @@
 return {
-    'nvim-telescope/telescope.nvim', branch = '0.1.x',
-    dependencies = { 'nvim-lua/plenary.nvim' },
-    config = function()
-        require('telescope').setup({})
+  'nvim-telescope/telescope.nvim',
+  branch = '0.1.x',
+  dependencies = { 
+    'nvim-lua/plenary.nvim',
+  },
+  config = function()
+    local telescope = require('telescope')
 
-        local builtin = require('telescope.builtin')
-        vim.keymap.set('n', '<leader><leader>', builtin.find_files, {})
-        vim.keymap.set('n', '<leader>/', builtin.live_grep, {})
-    end}
+    telescope.setup({})
+
+    local builtin = require('telescope.builtin')
+    vim.keymap.set('n', '<leader><leader>', builtin.find_files, {})
+    vim.keymap.set('n', '<leader>/', builtin.live_grep, {})
+  end,
+}
+
