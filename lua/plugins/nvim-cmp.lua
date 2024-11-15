126223f0a5c8b2128881032be7e1df503dd872a3
jpope6 <jaredpope06@gmail.com>
2024-11-15 10:30:01 -0800

    format everything

--- lua/plugins/nvim-cmp.lua
+++ lua/plugins/nvim-cmp.lua
@@ -1,133 +1,132 @@
 return {
-    "neovim/nvim-lspconfig",
-    dependencies = {
-        "williamboman/mason.nvim",
-        "williamboman/mason-lspconfig.nvim",
-        "hrsh7th/cmp-nvim-lsp",
-        "hrsh7th/cmp-buffer",
-        "hrsh7th/cmp-path",
-        "hrsh7th/cmp-cmdline",
-        "hrsh7th/nvim-cmp",
-        "L3MON4D3/LuaSnip",
-        "saadparwaiz1/cmp_luasnip",
-        "j-hui/fidget.nvim",
-        {
-            "tzachar/cmp-tabnine",
-            build = (vim.loop.os_uname().sysname == "Windows_NT") and "pwsh -noni .\\install.ps1" or "./install.sh",
-            config = function()
-                require("cmp_tabnine.config"):setup({
-                    max_lines = 1000,
-                    max_num_results = 3,
-                    sort = true,
-                })
-            end,
-        },
-    },
-    config = function()
-        local cmp = require("cmp")
-        local cmp_lsp = require("cmp_nvim_lsp")
-        local capabilities = vim.tbl_deep_extend(
-            "force",
-            {},
-            vim.lsp.protocol.make_client_capabilities(),
-            cmp_lsp.default_capabilities()
-        )
+	"neovim/nvim-lspconfig",
+	dependencies = {
+		"williamboman/mason.nvim",
+		"williamboman/mason-lspconfig.nvim",
+		"hrsh7th/cmp-nvim-lsp",
+		"hrsh7th/cmp-buffer",
+		"hrsh7th/cmp-path",
+		"hrsh7th/cmp-cmdline",
+		"hrsh7th/nvim-cmp",
+		"L3MON4D3/LuaSnip",
+		"saadparwaiz1/cmp_luasnip",
+		"j-hui/fidget.nvim",
+		{
+			"tzachar/cmp-tabnine",
+			build = (vim.loop.os_uname().sysname == "Windows_NT") and "pwsh -noni .\\install.ps1" or "./install.sh",
+			config = function()
+				require("cmp_tabnine.config"):setup({
+					max_lines = 1000,
+					max_num_results = 3,
+					sort = true,
+				})
+			end,
+		},
+	},
+	config = function()
+		local cmp = require("cmp")
+		local cmp_lsp = require("cmp_nvim_lsp")
+		local capabilities = vim.tbl_deep_extend(
+			"force",
+			{},
+			vim.lsp.protocol.make_client_capabilities(),
+			cmp_lsp.default_capabilities()
+		)
 
-        -- Setup fidget and mason
-        require("fidget").setup({})
-        require("mason").setup()
-        require("mason-lspconfig").setup({
-            ensure_installed = { "lua_ls", "rust_analyzer" },
-            handlers = {
-                -- Default handler for LSP servers
-                function(server_name)
-                    require("lspconfig")[server_name].setup({
-                        capabilities = capabilities,
-                    })
-                end,
-                -- Custom setup for ZLS
-                zls = function()
-                    local lspconfig = require("lspconfig")
-                    lspconfig.zls.setup({
-                        root_dir = lspconfig.util.root_pattern(".git", "build.zig", "zls.json"),
-                        settings = {
-                            zls = {
-                                enable_inlay_hints = true,
-                                enable_snippets = true,
-                                warn_style = true,
-                            },
-                        },
-                    })
-                    vim.g.zig_fmt_parse_errors = 0
-                    vim.g.zig_fmt_autosave = 0
-                end,
-                -- Custom setup for LuaLS
-                ["lua_ls"] = function()
-                    local lspconfig = require("lspconfig")
-                    lspconfig.lua_ls.setup({
-                        capabilities = capabilities,
-                        settings = {
-                            Lua = {
-                                runtime = { version = "Lua 5.1" },
-                                diagnostics = {
-                                    globals = { "bit", "vim", "it", "describe", "before_each", "after_each" },
-                                },
-                            },
-                        },
-                    })
-                end,
-            },
-        })
+		-- Setup fidget and mason
+		require("fidget").setup({})
+		require("mason").setup()
+		require("mason-lspconfig").setup({
+			ensure_installed = { "lua_ls", "rust_analyzer" },
+			handlers = {
+				-- Default handler for LSP servers
+				function(server_name)
+					require("lspconfig")[server_name].setup({
+						capabilities = capabilities,
+					})
+				end,
+				-- Custom setup for ZLS
+				zls = function()
+					local lspconfig = require("lspconfig")
+					lspconfig.zls.setup({
+						root_dir = lspconfig.util.root_pattern(".git", "build.zig", "zls.json"),
+						settings = {
+							zls = {
+								enable_inlay_hints = true,
+								enable_snippets = true,
+								warn_style = true,
+							},
+						},
+					})
+					vim.g.zig_fmt_parse_errors = 0
+					vim.g.zig_fmt_autosave = 0
+				end,
+				-- Custom setup for LuaLS
+				["lua_ls"] = function()
+					local lspconfig = require("lspconfig")
+					lspconfig.lua_ls.setup({
+						capabilities = capabilities,
+						settings = {
+							Lua = {
+								runtime = { version = "Lua 5.1" },
+								diagnostics = {
+									globals = { "bit", "vim", "it", "describe", "before_each", "after_each" },
+								},
+							},
+						},
+					})
+				end,
+			},
+		})
 
-        local cmp_select = { behavior = cmp.SelectBehavior.Select }
+		local cmp_select = { behavior = cmp.SelectBehavior.Select }
 
-        cmp.setup({
-            snippet = {
-                expand = function(args)
-                    require("luasnip").lsp_expand(args.body) -- For luasnip users
-                end,
-            },
-            mapping = cmp.mapping.preset.insert({
-                ["<C-j>"] = cmp.mapping.select_next_item(cmp_select),
-                ["<C-k>"] = cmp.mapping.select_prev_item(cmp_select),
-                ["<C-l>"] = cmp.mapping.confirm({ select = true }),
-                ["<CR>"] = cmp.mapping(function(fallback)
-                    if cmp.visible() then
-                        cmp.confirm({ select = true }) -- Confirm the selection if the menu is open
-                    else
-                        fallback() -- Default behavior of Enter (e.g., newline)
-                    end
-                end, { "i", "s" }), -- Works in insert and select modes
-                ["<C-e>"] = cmp.mapping.abort(),
-            }),
-            sources = cmp.config.sources({
-                { name = "cmp_tabnine", group_index = 1, priority = 100 },
-                { name = "nvim_lsp" },
-                { name = "luasnip" }, -- For luasnip users
-            }, {
-                { name = "buffer" },
-            }),
-            formatting = {
-                format = function(entry, item)
-                    if entry.source.name == "cmp_tabnine" then
-                        item.menu = "" -- Hide percentage in the menu for Tabnine
-                    end
-                    return item
-                end,
-            },
-        })
+		cmp.setup({
+			snippet = {
+				expand = function(args)
+					require("luasnip").lsp_expand(args.body) -- For luasnip users
+				end,
+			},
+			mapping = cmp.mapping.preset.insert({
+				["<C-j>"] = cmp.mapping.select_next_item(cmp_select),
+				["<C-k>"] = cmp.mapping.select_prev_item(cmp_select),
+				["<C-l>"] = cmp.mapping.confirm({ select = true }),
+				["<CR>"] = cmp.mapping(function(fallback)
+					if cmp.visible() then
+						cmp.confirm({ select = true }) -- Confirm the selection if the menu is open
+					else
+						fallback()                     -- Default behavior of Enter (e.g., newline)
+					end
+				end, { "i", "s" }),                    -- Works in insert and select modes
+				["<C-e>"] = cmp.mapping.abort(),
+			}),
+			sources = cmp.config.sources({
+				{ name = "cmp_tabnine", group_index = 1, priority = 100 },
+				{ name = "nvim_lsp" },
+				{ name = "luasnip" }, -- For luasnip users
+			}, {
+				{ name = "buffer" },
+			}),
+			formatting = {
+				format = function(entry, item)
+					if entry.source.name == "cmp_tabnine" then
+						item.menu = "" -- Hide percentage in the menu for Tabnine
+					end
+					return item
+				end,
+			},
+		})
 
-        -- Diagnostic settings
-        vim.diagnostic.config({
-            float = {
-                focusable = false,
-                style = "minimal",
-                border = "rounded",
-                source = "always",
-                header = "",
-                prefix = "",
-            },
-        })
-    end,
+		-- Diagnostic settings
+		vim.diagnostic.config({
+			float = {
+				focusable = false,
+				style = "minimal",
+				border = "rounded",
+				source = "always",
+				header = "",
+				prefix = "",
+			},
+		})
+	end,
 }
-
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,17 +1,16 @@
 return {
-  'nvim-telescope/telescope.nvim',
-  branch = '0.1.x',
-  dependencies = { 
-    'nvim-lua/plenary.nvim',
-  },
-  config = function()
-    local telescope = require('telescope')
+	'nvim-telescope/telescope.nvim',
+	branch = '0.1.x',
+	dependencies = {
+		'nvim-lua/plenary.nvim',
+	},
+	config = function()
+		local telescope = require('telescope')
 
-    telescope.setup({})
+		telescope.setup({})
 
-    local builtin = require('telescope.builtin')
-    vim.keymap.set('n', '<leader><leader>', builtin.find_files, {})
-    vim.keymap.set('n', '<leader>/', builtin.live_grep, {})
-  end,
+		local builtin = require('telescope.builtin')
+		vim.keymap.set('n', '<leader><leader>', builtin.find_files, {})
+		vim.keymap.set('n', '<leader>/', builtin.live_grep, {})
+	end,
 }
-
