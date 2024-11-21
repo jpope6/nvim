7d68c902c75297ebc0a002857865177fac0271aa
jpope6 <jaredpope06@gmail.com>
2024-11-21 11:46:39 -0800

    change theme

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -3,7 +3,9 @@ return {
 	lazy = false, -- make sure we load this during startup if it is your main colorscheme
 	priority = 1000, -- make sure to load this before all the other start plugins
 	config = function()
-		require("onedark").setup({})
+		require("onedark").setup({
+			style = "warm",
+		})
 		require("onedark").load()
 	end,
 }
